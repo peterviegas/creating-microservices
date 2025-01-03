7713f7d738b75fc5bed35a16d36b0f20e0ef865d
Peter Viegas <peterviegas@outlook.com>
2025-01-03 11:25:19 -0300

    Atualization of controller

--- src/main/java/com/peter/pedidos/api/controller/PedidoController.java
+++ src/main/java/com/peter/pedidos/api/controller/PedidoController.java
@@ -11,11 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.peter.pedidos.api.entity.Pedido;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+@Tag(name = "Pedidos", description = "Resource for creade a new Order")
 @RestController
 @RequestMapping("/api/v1/pedidos")
 public class PedidoController {
 	private final Logger logger = LoggerFactory.getLogger(PedidoController.class);
 	
+	@Operation(summary = "Create a ne Order", description = "Have the operations for creat a new order",
+			responses = @ApiResponse(responseCode = "201", description = "Resource created with success",
+			content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class)))) 
 	@PostMapping
 	public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido){
 		logger.info("Pedido recebido request: {}", pedido);
@@ -24,5 +35,4 @@ public class PedidoController {
 		logger.info("Pedido recebido:{}", pedido.toString());
 		return ResponseEntity.status(HttpStatus.CREATED).body(pedido);
 	}
-
 }
