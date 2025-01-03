31430d1c735861cb765fbe70f940af232f6f7f9d
Peter <peterviegas@outlook.com>
2025-01-03 11:02:19 +0000

    Chanve environment

---
+++ src/main/java/com/peter/pedidos/api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.peter.pedidos.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+				.info(
+						new Info()
+							.title("Rest API - Pedidos")
+							.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+							.contact(new Contact().name("Peter Viegas").email("peterviegas@outlook.com"))
+						);
+	}
+}
